1cfc40f1cc574fc0fa4522ca6b46fe5cdcb54c53
RonitD631 <dhalronit631@gmail.com>
2025-06-29 22:56:37 +0530

    project structure initial commit

---
+++ src/main/java/com/kafka_03/springboot_kafka_integration/SpringbootKafkaIntegrationApplication.java
@@ -0,0 +1,13 @@
+package com.kafka_03.springboot_kafka_integration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootKafkaIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootKafkaIntegrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kafka_03/springboot_kafka_integration/SpringbootKafkaIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kafka_03.springboot_kafka_integration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootKafkaIntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
